453b984f6e41f5f628a7036aedfff06e19f69a57
JavaScript Sandbox <jessedrobertson@gmail.com>
2015-07-27 03:32:42 +0000

    initial

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://brash-shmoes:revlis^14@ds027483.mongolab.com:27483/brash-shmoes'
+  },
+
+  seedDB: true
+};
